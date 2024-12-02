3baf08577be8cc97280ea5b51e3d56c87291e24f
A McVey Moffatt <amcveym@emory.edu>
2024-12-02 09:23:14 -0500

    fix error with broom.helpers

--- Code/00_renv.R
+++ Code/00_renv.R
@@ -3,7 +3,9 @@ if(("renv" %in% row.names(installed.packages()))==FALSE) {
   install.packages("renv")
   renv::activate()
   renv::restore()
+  install.packages("broom.helpers")
   } else {
     renv::activate()
     renv::restore()
+    install.packages("broom.helpers")
   }
